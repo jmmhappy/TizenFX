466911839fa9cbcc440585b133ccf4eb61cfc633
Seoyeon2Kim <34738918+Seoyeon2Kim@users.noreply.github.com>
2022-09-26 17:37:35 +0900

    Revert "[NUI] revert "[NUI] Modify the return value of OnAccessibilityPan()." (#4599)"
    This reverts commit e5623567b110e2f58d3e659951d4ffaa36d2276c.

--- src/Tizen.NUI.Components/Controls/ScrollableBase.cs
+++ src/Tizen.NUI.Components/Controls/ScrollableBase.cs
@@ -1380,17 +1380,7 @@ namespace Tizen.NUI.Components
 
         internal override bool OnAccessibilityPan(PanGesture gestures)
         {
-            if (SnapToPage && scrollAnimation != null && scrollAnimation.State == Animation.States.Playing)
-            {
-                return false;
-            }
-
-            if (mScrollEnabled == true)
-            {
-                OnPanGesture(gestures);
-            }
-
-            return true;
+            return (mScrollEnabled ? OnPanGesture(gestures) : false);
         }
 
         private float CustomScrollAlphaFunction(float progress)
